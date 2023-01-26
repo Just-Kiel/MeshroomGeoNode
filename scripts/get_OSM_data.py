a89ceb777bfcf0906cbb3a85e648242ebd62e82c
olivia-ddow <oli-acha01@laposte.net>
2023-01-26 09:19:42 +0100

    added water in osm_data and added requirements.txt

--- scripts/get_OSM_data.py
+++ scripts/get_OSM_data.py
@@ -52,11 +52,22 @@ roads = ox.plot_figure_ground(
 )
 
 # TODO water path
+fp = path + f"waterways.{extension}"
+tags = {"natural": "water"}
+gdf = ox.geometries_from_point(point, tags, dist=dist)
+waterways = ox.plot_footprints(
+    gdf,
+    color="navy",
+    save=True,
+    filepath=fp,
+    show=False,
+    dpi=dpi
+)
 
 
 fp = path + f"buildings.{extension}"
 #### Get buildings layer
-tags = {"building":True, "water":True}
+tags = {"building":True}
 gdf = ox.geometries_from_point(point, tags, dist=dist)
 buildings = ox.plot_footprints(
     gdf,
@@ -68,9 +79,12 @@ buildings = ox.plot_footprints(
 
 buildings = path + f"buildings.{extension}"
 roads = path + f"roads.{extension}"
+waterways = path + f"waterways.{extension}"
 
 image1 = Image.open(buildings)
 image2 = Image.open(roads)
+image3 = Image.open(waterways)
+
 
 # resize image before cropping the other one
 ratio = image2.width / image2.height
@@ -87,7 +101,8 @@ image1 = image1.crop(crop_size)
 image1.save(buildings)
 image1 = Image.open(buildings)
 
-
 # merging of 2 images (layers)
-image3 = ImageChops.add(image2, image1)
-image3 = image3.save(finalFp)
+image4 = ImageChops.add(image2, image1)
+image4 = image4.save(finalFp)
+#imgef = ImageChops.add(image4,image3)
+#imgef = imgef.save(finalFp)
